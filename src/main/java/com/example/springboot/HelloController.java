e277291499eb0bf2fe6ec7778ed3f4488ef38fb4
Veer Muchandi <veer.muchandi@gmail.com>
2023-01-03 22:49:18 +0000

    added kustomize files and target env

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,18 @@
 package com.example.springboot;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+	@Value("${location:local}")
+    String location;
+
 	@GetMapping("/")
 	public String index() {
-        String message="Hello from Spring Boot!";
+        String message= String.format("Hello from Spring Boot running in %s!", location);
 		return message;
 	}
 
