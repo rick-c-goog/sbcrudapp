1303077fccce76b23971c968a24d03b9590d6f38
rick-cloudcode <admin@rickruguichen.altostrat.com>
2023-03-07 17:49:55 +0000

    Update Dockerfile

--- src/main/java/com/example/springboot/Item.java
+++ src/main/java/com/example/springboot/Item.java
@@ -2,11 +2,11 @@ package com.example.springboot;
 
 import java.math.BigDecimal;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Item {
