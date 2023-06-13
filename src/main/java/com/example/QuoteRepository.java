694c95d34148e5197898503802867c86f1efee5d
rick-cloudcode <admin@rickruguichen.altostrat.com>
2023-06-13 15:44:01 +0000

    fix order by RAND() issue

--- src/main/java/com/example/QuoteRepository.java
+++ src/main/java/com/example/QuoteRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface QuoteRepository extends JpaRepository<Quote, Integer> {
 
-  @Query(nativeQuery = true, value = "SELECT id,quote,author FROM quotes ORDER BY RANDOM() LIMIT 1")
+  @Query(nativeQuery = true, value = "SELECT id,quote,author FROM quotes ORDER BY RAND() LIMIT 1")
   Quote findRandomQuote();
 }
